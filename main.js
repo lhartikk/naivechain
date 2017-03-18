bd82e39f8ce57bdda009195c0ea42cc051b2e017
Santiago Siri <santisiri@gmail.com>
2017-03-18 00:01:47 -0400

    a nicer beginning

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ var MessageType = {
 };
 
 var getGenesisBlock = () => {
-    return new Block(0, "0", 1465154705, "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
+    return new Block(0, "0", 1465154705, "let there be light.", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
 };
 
 var blockchain = [getGenesisBlock()];
