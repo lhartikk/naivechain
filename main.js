0d7a66f3103df9cd8b21db0acaaa7bf061a69d1e
thoffmann <toni.hoffmann@arthrex.de>
2017-12-07 15:27:12 +0100

    implementing nonce

--- main.js
+++ main.js
@@ -9,12 +9,13 @@ var p2p_port = process.env.P2P_PORT || 6001;
 var initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
 class Block {
-    constructor(index, previousHash, timestamp, data, hash) {
+    constructor(index, previousHash, timestamp, data, hash, nonce) {
         this.index = index;
         this.previousHash = previousHash.toString();
         this.timestamp = timestamp;
         this.data = data;
         this.hash = hash.toString();
+        this.nonce = nonce;
     }
 }
 
@@ -26,7 +27,7 @@ var MessageType = {
 };
 
 var getGenesisBlock = () => {
-    return new Block(0, "0", 1465154705, "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
+    return new Block(0, "0", 1465154705, "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7", 0);
 };
 
 var blockchain = [getGenesisBlock()];
@@ -100,21 +101,26 @@ var generateNextBlock = (blockData) => {
     var previousBlock = getLatestBlock();
     var nextIndex = previousBlock.index + 1;
     var nextTimestamp = new Date().getTime() / 1000;
-    var nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
-    return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash);
+    var calculatedHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
+    var nextHash = calculatedHash.hash;
+    var nextNonce = calculatedHash.nonce;
+    return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash, nextNonce);
 };
 
 
 var calculateHashForBlock = (block) => {
-    return calculateHash(block.index, block.previousHash, block.timestamp, block.data);
+    return calculateHash(block.index, block.previousHash, block.timestamp, block.data, block.nonce).hash;
 };
 
-var calculateHash = (index, previousHash, timestamp, data) => {
+var calculateHash = (index, previousHash, timestamp, data, nonce) => {
+    if(nonce){
+        return {hash: CryptoJS.SHA256(index + previousHash + timestamp + data + nonce).toString(), nonce: nonce};
+    }
     while(true){
-        var nonce = Math.random();
+        nonce = Math.random();
         var hash = CryptoJS.SHA256(index + previousHash + timestamp + data + nonce).toString();
         if(hash.toString().startsWith("0000")){
-            return hash;
+            return {hash: hash, nonce: nonce};
         }
     }
 };
